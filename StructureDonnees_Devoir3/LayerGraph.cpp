de6e0fc6ab94f45b45b0102b21c9117aaa8f23f7
Edouard <edouard.samyn@gmail.com>
2016-11-15 22:14:42 -0500

    Fix in LayerGraph constructor, edge of Source was not functional

--- StructureDonnees_Devoir3/LayerGraph.cpp
+++ StructureDonnees_Devoir3/LayerGraph.cpp
@@ -3,12 +3,7 @@
 
 LayerGraph::LayerGraph(AFDGraph graph, int wordLength)
 {
-	//Source
-	vector<Edge> edges = vector<Edge>();
-	Edge edge = Edge(make_shared<State>(graph.getStartState()), "", 0);
-	edges.push_back(edge);
-	this->source = State(-1, false, edges);
-
+	
 	//Layers
 	this->layers = vector<vector<State>>();
 	this->layers.push_back(vector<State>());
@@ -17,6 +12,26 @@ LayerGraph::LayerGraph(AFDGraph graph, int wordLength)
 		this->layers[0].push_back(newState);
 	}
 
+	//Source
+	//Create Edge From Source To Start State of AFDGrpah (first Layer)
+	vector<Edge> edges = vector<Edge>();
+	int idArrivalFromSource = graph.getStartState().getId();
+	bool find = false;
+	State arrivalState = State();
+	while (!find) {
+		vector<State>::iterator it;
+		for (it = this->layers[i].begin(); it != this->layers[i].end(); ++it) {
+			if (it->getId() == idArrivalFromSource) {
+				break;
+			}
+		}
+		arrivalState = *it;
+		find = true;
+	}
+	Edge edge = Edge(make_shared<State>(arrivalState), "", 0);
+	edges.push_back(edge);
+	this->source = State(-1, false, edges);
+
 	for (int i = 1; i < wordLength; i++) {
 		this->layers.push_back(vector<State>());
 		for (State state : graph.getStates()) {
@@ -24,6 +39,8 @@ LayerGraph::LayerGraph(AFDGraph graph, int wordLength)
 			this->layers[0].push_back(newState);
 		}
 
+
+		//Create Edges for each layer
 		for (State state : this->layers[i - 1]) {
 			State stateInAFDGraph = graph.getState(state.getId());
 			for (Edge edge : stateInAFDGraph.getTransitions()) {
